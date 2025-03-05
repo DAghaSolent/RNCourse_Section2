ad692ef6f9442b04ad3cd15d6b430b91f4421fc0
DAghaSolent <5aghad30@solent.ac.uk>
2025-03-05 12:50:38 +0000

    Basic Addition of Flexbox to style textviews and inputs nicely

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your Course Goal!'/>
+        <Button title = "Add Goal"/>
+      </View> 
       <View>
-        <Text style={styles.dummyText}>Another Piece of Text</Text>
-      </View>
-      <View>
-        <Text style={styles.dummyText}>Hello World!!!!</Text>
-         {/* <StatusBar style="auto" /> */}
-        <Button title='Tap Me!'/>
+        <Text>List of goals...</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    padding: 50
   },
-  dummyText:{
-    margin: 16, 
-    borderWidth: 2, 
-    borderColor: 'blue', 
-    padding: 16
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
   }
 });
